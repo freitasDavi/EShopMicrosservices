6760f01142b1b9a769e64c2718bfcbcf609dd6a4
Davi <davi.frrs@outlook.com>
2025-07-28 13:24:12 -0300

    feat: setup Carter and Mapster

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,17 +1,31 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-        : IRequest<CreateProductResult>;
+        : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            // create product entity from command object
+            var product = new Product
+            {
+                Category = request.Category,
+                Description = request.Description,
+                ImageFile = request.ImageFile,
+                Name = request.Name,
+                Price = request.Price,
+            };
+
+            // save to database
+
+            // return CreateProductResult
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+// Map all classes that implements ICarterModule
+app.MapCarter();
 
 app.Run();
