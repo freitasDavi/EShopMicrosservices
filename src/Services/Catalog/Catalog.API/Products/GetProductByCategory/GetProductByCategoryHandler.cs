5311509d63c482a29d025f8f37645de11fb620a9
Davi <davi.frrs@outlook.com>
2025-08-15 08:49:55 -0300

    feat: Get product by category

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+    internal class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Request}", request);
+
+            var products = await session.Query<Product>()
+                .Where(p => p.Category.Contains(request.Category))
+                .ToListAsync(cancellationToken);
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
