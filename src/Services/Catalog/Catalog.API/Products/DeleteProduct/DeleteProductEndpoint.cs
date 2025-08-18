7e066f89b6707419989115a3466ca0f48b16731a
Davi <davi.frrs@outlook.com>
2025-08-18 09:08:00 -0300

    feat: Delete

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct
+{
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        // public record DeleteProductRequest (Guid id);
+
+        public record DeleteProductResponse(bool isSuccess);
+
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(Id));
+
+                var response = result.Adapt<DeleteProductResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("DeleteProduct")
+                .WithDescription("Delete a product")
+                .WithSummary("Delete a product")
+                .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id): ICommand<DeleteProductResult>;
+
+    public record DeleteProductResult(bool IsSuccess);
+
+    internal class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Handling DeleteProductCommand for product with ID: {ProductId}", request.Id);
+
+            session.Delete<Product>(request.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+    }
+}
